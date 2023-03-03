78843601ebbcab62ac6123a07b9b26e5ae747a7b
Richard Chalk <barmybrit@gmail.com>
2023-03-03 11:57:59 +0100

    Done with Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,33 @@
 import React from "react";
 import { Navbar } from "./Navbar";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import { FiArrowRight } from "react-icons/fi";
 
 export const Home = () => {
   return (
     <div className="home-container">
       <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Din favorit mat levererat hem till din dörr!
+          </h1>
+          <p className="primary-text">
+            Utbildade kockar gör all preperationsarbetet så du kan enkelt
+            förberada god, prisvärt & nyttig mat.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
