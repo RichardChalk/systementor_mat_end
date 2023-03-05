4a2c533e1b75c0d855463fd093ec274d566c8fba
Richard Chalk <barmybrit@gmail.com>
2023-03-05 20:36:11 +0100

    Done with Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { About } from "./Components/About";
+import { Contact } from "./Components/Contact";
+import { Footer } from "./Components/Footer";
 import { Home } from "./Components/Home";
 import { Testominial } from "./Components/Testominial";
 import { Work } from "./Components/Work";
@@ -11,6 +13,8 @@ function App() {
       <About />
       <Work />
       <Testominial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
