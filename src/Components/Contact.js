774080eca491bb7ce7240dca45da5ea16fe89480
Richard Chalk <barmybrit@gmail.com>
2023-03-06 06:32:55 +0100

    Added action method on contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,10 +6,14 @@ export const Contact = () => {
     <div className="contact-page-wrapper" id="contact">
       <h1 className="primary-heading">Har du en fråga?</h1>
       <h1 className="primary-heading">Låt oss hjälpa dig</h1>
-      <div className="contact-form-container">
-        <input type="text" placeholder="yourmail@gmail.com" />
-        <button className="secondary-button">Skicka</button>
-      </div>
+      <form
+        action="https://formshow.systementor.se/"
+        method="post"
+        className="contact-form-container"
+      >
+        <input type="text" name="email" placeholder="yourmail@gmail.com" />
+        <button className="secondary-button input-email">Skicka</button>
+      </form>
     </div>
   );
 };
